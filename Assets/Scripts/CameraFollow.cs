aee9abe15ba2db1e151cd3b6ad602981d5716391
Masab12 <masabfarooque1122@gmail.com>
2023-02-27 04:54:09 +0500

    Winter Game Jam 2023
    Winter Game Jam 2023

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,17 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
+    public Camera zoomedInCamera;
+    public Camera zoomedOutCamera;
+    public Button zoomButton;
 
-    // Update is called once per frame
+    private Vector3 defaultPosition;
+    private bool isZoomedOut = false;
+
+    void Start()
+    {
+        defaultPosition = zoomedInCamera.transform.position;
+        zoomedOutCamera.enabled = false;
+    }
+
     void Update()
     {
-        if (CharacterMovement.isMoving)
+        if (!isZoomedOut)
         {
-            transform.position = new Vector3(transform.position.x, player.position.y + 7f, player.position.z + 1f);
+            if (CharacterMovement.isMoving)
+            {
+                zoomedInCamera.transform.position = new Vector3(transform.position.x, player.position.y + 7f, player.position.z + 1f);
+            }
+        }
+    }
+
+    public void ToggleZoom()
+    {
+        if (isZoomedOut)
+        {
+            zoomedInCamera.enabled = true;
+            zoomedOutCamera.enabled = false;
+            zoomedInCamera.transform.position = defaultPosition;
+            isZoomedOut = false;
+        }
+        else
+        {
+            zoomedInCamera.enabled = false;
+            zoomedOutCamera.enabled = true;
+            isZoomedOut = true;
         }
     }
 }
---
+++ Assets/Scripts/RoomPuzzle.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+public class RoomPuzzle : MonoBehaviour
+{
+    public Transform[] roomPositions;
+
+    public Transform[] rooms;
+
+    private Transform currentRoom;
+    public Transform emptySpace;
+
+    public bool isDragging = false;
+    public Vector3 dragStartPos;
+    public Vector3 currentRoomOffset;
+
+    public Camera zoomedOutCamera;
+
+    void Start()
+    {
+        rooms = new Transform[3];
+        rooms[0] = GameObject.Find("Room1").transform;
+        rooms[1] = GameObject.Find("Room2").transform;
+        rooms[2] = GameObject.Find("Room3").transform;
+
+        currentRoom = rooms[0];
+        emptySpace = GameObject.Find("EmptySpace").transform;
+    }
+
+    void Update()
+    {
+        if (zoomedOutCamera.enabled) // Only allow puzzle to be moved when zoomedOutCamera is enabled
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+                {
+                    for (int i = 0; i < rooms.Length; i++)
+                    {
+                        if (hit.transform == rooms[i])
+                        {
+                            isDragging = true;
+                            dragStartPos = hit.point;
+                            currentRoomOffset = rooms[i].position - dragStartPos;
+                            break;
+                        }
+                    }
+                }
+            }
+            else if (Input.GetMouseButton(0) && isDragging)
+            {
+                Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + currentRoomOffset;
+                Vector3 emptySpacePosition = emptySpace.position;
+                float distanceToEmptySpace = Vector3.Distance(newPosition, emptySpacePosition);
+
+                if (distanceToEmptySpace < 40f)
+                {
+                    currentRoom.position = newPosition;
+                }
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                isDragging = false;
+
+                bool isSolved = true;
+                for (int i = 0; i < rooms.Length; i++)
+                {
+                    float distanceToEmptySpace = Vector3.Distance(rooms[i].position, emptySpace.position);
+                    if (distanceToEmptySpace < 40f)
+                    {
+                        Vector3 tempPosition = rooms[i].position;
+                        rooms[i].position = emptySpace.position;
+                        emptySpace.position = tempPosition;
+                    }
+                    if (rooms[i] != emptySpace && rooms[i].position != roomPositions[i].position)
+                    {
+                        isSolved = false;
+                    }
+                }
+
+                if (isSolved)
+                {
+                    Debug.Log("Rooms Moving!");
+                }
+            }
+        }
+    }
+}
