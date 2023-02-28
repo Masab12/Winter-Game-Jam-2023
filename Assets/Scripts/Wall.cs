41ab3590299013df627b9d5a703a5c9aaf0189bb
Masab12 <masabfarooque1122@gmail.com>
2023-02-28 20:28:48 +0500

    Effects Added
    Effects Added

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -6,14 +6,31 @@ public class Wall : MonoBehaviour
 {
     public GameObject connectedWall;
     public string wallTag;
+    public ParticleSystem particleEffect;
+    public ParticleSystem particleEffect2;// Add a reference to the particle system
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag(wallTag))
         {
-            Debug.Log("Tags match");
             connectedWall.SetActive(false);
             gameObject.SetActive(false);
+            if (particleEffect != null) // Check if the particle effect reference is not null
+            {
+                // Instantiate the particle effect at the current position and rotation of the wall
+                ParticleSystem effect = Instantiate(particleEffect, transform.position, transform.rotation);
+                // Set the effect to play and destroy itself after it has finished playing
+                effect.Play();
+                Destroy(effect.gameObject, effect.main.duration);
+            }
+            if (particleEffect != null) // Check if the particle effect reference is not null
+            {
+                // Instantiate the particle effect at the current position and rotation of the wall
+                ParticleSystem effect = Instantiate(particleEffect2, transform.position, transform.rotation);
+                // Set the effect to play and destroy itself after it has finished playing
+                effect.Play();
+                Destroy(effect.gameObject, effect.main.duration);
+            }
         }
     }
 }
