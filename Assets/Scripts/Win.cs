f4997b70cb1aac29980c0690895e032f8f222bd1
Masab12 <masabfarooque1122@gmail.com>
2023-03-01 20:07:59 +0500

    3 Levels Done
    3 Levels Done

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -18,15 +18,13 @@ public class Win : MonoBehaviour
             Debug.Log("You win!");
             FindObjectOfType<AudioManager>().PlaySound("Win");
 
-            if (particleEffect != null) // Check if the particle effect reference is not null
-            {
-                FindObjectOfType<AudioManager>().PlaySound("Collide");
-                // Instantiate the particle effect at the current position and rotation of the wall
+              
+             
                 ParticleSystem effect = Instantiate(particleEffect, transform.position, transform.rotation);
-                // Set the effect to play and destroy itself after it has finished playing
+                
                 effect.Play();
                 Destroy(effect.gameObject, effect.main.duration);
-            }
+            
             characters.gameObject.SetActive(false);
             endScene.SetActive(true);
             Invoke(nameof(ShowCompletePanel), 5f);
