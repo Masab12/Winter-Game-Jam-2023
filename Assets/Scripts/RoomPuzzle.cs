064489e1e89a067140eb2b1c352c5043544a4b22
Masab12 <masabfarooque1122@gmail.com>
2023-03-03 20:07:56 +0500

    Level 4 Added, Rooms are added in realtime through tags
    Winter Game Jam 2023

--- Assets/Scripts/RoomPuzzle.cs
+++ Assets/Scripts/RoomPuzzle.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System.Collections.Generic;
 using System.Collections;
+
 public class RoomPuzzle : MonoBehaviour
 {
-    public Transform[] roomPositions;
-
-    public Transform[] rooms;
+    public List<Transform> roomPositions = new List<Transform>();
+    public List<Transform> rooms = new List<Transform>();
 
     private Transform currentRoom;
     public Transform emptySpace;
@@ -18,105 +19,105 @@ public class RoomPuzzle : MonoBehaviour
 
     public Camera zoomedOutCamera;
 
-void Start()
+    void Start()
     {
-
-        rooms = new Transform[3];
-        rooms[0] = GameObject.Find("Room1").transform;
-        rooms[1] = GameObject.Find("Room2").transform;
-        rooms[2] = GameObject.Find("Room3").transform;
+        foreach (GameObject roomObject in GameObject.FindGameObjectsWithTag("Room"))
+        {
+            Transform room = roomObject.transform;
+            rooms.Add(room.transform);
+        }
 
         currentRoom = rooms[0];
         emptySpace = GameObject.Find("EmptySpace").transform;
     }
-        void Update()
-        {
-            Joystick.SetActive(true);
-            if (zoomedOutCamera.enabled) // Only allow puzzle to be moved when zoomedOutCamera is enabled
-            {
-                Joystick.SetActive(false);
 
-                if (EventSystem.current.IsPointerOverGameObject()) // Check if the mouse pointer is over a UI element
+    void Update()
+    {
+        Joystick.SetActive(true);
+        if (zoomedOutCamera.enabled) // Only allow puzzle to be moved when zoomedOutCamera is enabled
+        {
+            Joystick.SetActive(false);
+
+            if (EventSystem.current.IsPointerOverGameObject()) // Check if the mouse pointer is over a UI element
+            {
+                return; // Ignore input if over UI
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
                 {
-                    return; // Ignore input if over UI
-                }
-                if (Input.GetMouseButtonDown(0))
-                {
-                    RaycastHit hit;
-                    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+                    foreach (Transform room in rooms)
                     {
-                        for (int i = 0; i < rooms.Length; i++)
+                        if (hit.transform == room)
                         {
-                            if (hit.transform == rooms[i])
-                            {
-                                isDragging = true;
-                                dragStartPos = hit.point;
-                                currentRoomOffset = rooms[i].position - dragStartPos;
-                                break;
-                            }
+                            isDragging = true;
+                            dragStartPos = hit.point;
+                            currentRoomOffset = room.position - dragStartPos;
+                            break;
                         }
                     }
                 }
-                /*else if (Input.GetMouseButton(0) && isDragging)
-                {
-                    Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + currentRoomOffset;
-                    Vector3 emptySpacePosition = emptySpace.position;
-                    float distanceToEmptySpace = Vector3.Distance(newPosition, emptySpacePosition);
+            }
+            else if (Input.GetMouseButton(0) && isDragging)
+            {
+                Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + currentRoomOffset;
+                Vector3 emptySpacePosition = emptySpace.position;
+                float distanceToEmptySpace = Vector3.Distance(newPosition, emptySpacePosition);
 
+                if (distanceToEmptySpace < 40f)
+                {
+                    currentRoom.position = newPosition;
+                }
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                isDragging = false;
+
+                bool isSolved = true;
+                for (int i = 0; i < rooms.Count; i++)
+                {
+                    float distanceToEmptySpace = Vector3.Distance(rooms[i].position, emptySpace.position);
                     if (distanceToEmptySpace < 40f)
                     {
-                        currentRoom.position = newPosition;
+                        Vector3 tempPosition = rooms[i].position;
+                        rooms[i].position = emptySpace.position;
+                        emptySpace.position = tempPosition;
                     }
-                }*/
-                else if (Input.GetMouseButtonUp(0))
+                    if (rooms[i] != emptySpace && rooms[i].position != roomPositions[i].position)
+                    {
+                        isSolved = false;
+                    }
+                }
+
+                if (isSolved)
                 {
-                    isDragging = false;
-
-                    bool isSolved = true;
-                    for (int i = 0; i < rooms.Length; i++)
-                    {
-                        float distanceToEmptySpace = Vector3.Distance(rooms[i].position, emptySpace.position);
-                        if (distanceToEmptySpace < 40f)
-                        {
-                            Vector3 tempPosition = rooms[i].position;
-                            rooms[i].position = emptySpace.position;
-                            emptySpace.position = tempPosition;
-                        }
-                        if (rooms[i] != emptySpace && rooms[i].position != roomPositions[i].position)
-                        {
-                            isSolved = false;
-                        }
-                    }
-
-                    if (isSolved)
-                    {
-                        Debug.Log("Rooms Moving!");
-                        zoomedOutCamera.enabled = true;
-                        StartCoroutine(ShakeCamera());
-                    }
+                    Debug.Log("Rooms Moving!");
+                    zoomedOutCamera.enabled = true;
+                    StartCoroutine(ShakeCamera());
                 }
             }
         }
-        IEnumerator ShakeCamera()
-                {
-                    float duration = 0.5f;
-                    float magnitude = 0.2f;
+    }
 
-                    Vector3 originalPosition = Camera.main.transform.position;
+    IEnumerator ShakeCamera()
+    {
+        float duration = 0.5f;
+        float magnitude = 0.2f;
 
-                    float elapsed = 0.0f;
-                    while (elapsed < duration)
-                    {
-                        float x = Random.Range(-1f, 1f) * magnitude;
-                        float y = Random.Range(-1f, 1f) * magnitude;
+        Vector3 originalPosition = Camera.main.transform.position;
 
-                        Camera.main.transform.position = new Vector3(originalPosition.x + x, originalPosition.y + y, originalPosition.z);
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
 
-                        elapsed += Time.deltaTime;
+            Camera.main.transform.position = new Vector3(originalPosition.x + x, originalPosition.y + y, originalPosition.z);
 
-                        yield return null;
-                    }
-                }
-            }
-        
-   
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+    }
+}
